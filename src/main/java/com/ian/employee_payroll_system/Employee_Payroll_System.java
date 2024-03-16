647520c195e046f568d12feadde47cf432a312a9
iankristoper <iankristoper26@gmail.com>
2024-03-16 14:54:08 +0800

    UPDATE: added main class features

---
+++ src/main/java/com/ian/employee_payroll_system/Employee_Payroll_System.java
@@ -0,0 +1,18 @@
+package com.ian.employee_payroll_system;
+
+/**
+ *
+ * @author Ian Tagano
+ */
+
+
+
+public class Employee_Payroll_System 
+{
+
+    public static void main(String[] args) 
+    {
+      
+        
+    }
+}
