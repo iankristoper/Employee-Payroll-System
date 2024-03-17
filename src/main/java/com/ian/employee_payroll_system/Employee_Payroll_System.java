045991ba498b15798e221461c013aca32543f476
iankristoper <iankristoper26@gmail.com>
2024-03-17 10:58:01 +0800

    UPDATE: added system class features

--- src/main/java/com/ian/employee_payroll_system/Employee_Payroll_System.java
+++ src/main/java/com/ian/employee_payroll_system/Employee_Payroll_System.java
@@ -1,5 +1,7 @@
 package com.ian.employee_payroll_system;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Ian Tagano
@@ -12,7 +14,46 @@ public class Employee_Payroll_System
 
     public static void main(String[] args) 
     {
-      
+        Payroll_System theSystem = new Payroll_System();    //create new instance of the Payroll_system
+        Scanner scanner = new Scanner(System.in);
+        
+        int choice;
+        
+        do
+        {
+            System.out.println("\nEmployee Payroll System Menu:");
+            System.out.println("1. Add New Employee");
+            System.out.println("2. Calculate Bonus");
+            System.out.println("3. Display Employee Information");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+            
+            switch(choice)
+            {
+                case 1:
+                    theSystem.addEmployee();
+                    break;
+                    
+                case 2:
+                    theSystem.calculateBonus();
+                    break;
+                    
+                case 3:
+                    theSystem.displayEmployeeInfo();
+                    break;
+                    
+                case 4:
+                    System.out.println("Exiting system program.");
+                    break;
+                    
+                default:
+                    System.out.println("Invalid choice, please try again.");
+                    break;
+            }     
+        } while(choice != 4);
+       
+        
         
     }
 }
