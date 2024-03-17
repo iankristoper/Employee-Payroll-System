5429d3eb17191bf89c5c648b9fdaee0408a6eb2a
iankristoper <iankristoper26@gmail.com>
2024-03-17 10:29:15 +0800

    UPDATE: added employee class features

--- src/main/java/com/ian/employee_payroll_system/Employee.java
+++ src/main/java/com/ian/employee_payroll_system/Employee.java
@@ -8,6 +8,57 @@ package com.ian.employee_payroll_system;
 
 public class Employee 
 {
-    private final 
+    //private attributes for data hiding 
+    private String name;
+    private double salary;
+    private double bonusRate;
+    
+    public Employee(String name, double salary, double bonusRate)
+    {
+        this.name = name;
+        this.salary = salary;
+        this.bonusRate = bonusRate;
+    }
+    
+    
+    //this is the getter methods 
+    public String getName() {
+        return name;
+    }
+    
+    public double getSalary() {
+        return salary;
+    }
+    
+    public double getBonueRate() {
+        return bonusRate;
+    }
+    
+    
+    //this is the setter methods 
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    
+    public void setBonusRate(double bonusRate) {
+        this.bonusRate = bonusRate;
+    }
+    
+    public double calculateBonus(double bonusRate) {
+        return salary * bonusRate;
+    }
+    
+    
+    //
+    @Override 
+    public String toString()
+    {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", salary=" + salary +
+                ", bonusRate=" + bonusRate +
+                '}';
+    }
+    
     
 }
